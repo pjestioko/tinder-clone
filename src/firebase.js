1bbe5d402f23c8429c0f69f4557036c64560a4a1
pjestioko <pauljoseph.estioko@gmail.com>
2021-05-24 21:15:19 -1000

    added firebase congif

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyArvMbSm_-S9xwciYN8vk3Az4t9-9CWpKY",
+      authDomain: "tinder-clone-3c731.firebaseapp.com",
+      projectId: "tinder-clone-3c731",
+      storageBucket: "tinder-clone-3c731.appspot.com",
+      messagingSenderId: "480673097239",
+      appId: "1:480673097239:web:d2bbc174bf9540deee1e52",
+      measurementId: "G-JZF1TRZ3B0"
+    };
