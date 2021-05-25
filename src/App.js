bd7c5d278253eac0022719bd7a5ffb73eab28c2b
pjestioko <pauljoseph.estioko@gmail.com>
2021-05-24 19:43:24 -1000

    added router and switches

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import Header from './Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-
-      {/*   Header   */}
       <Header />
-      {/*   Tinder Cards */}
-      {/*   Buttons below Tinder cards */}
-
-      {/*   Chat screen   */}
-      {/*   Individual chat screen   */}
-
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am at chat page</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am at homepage</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+// {/* <Header />
+// {/*   Tinder Cards */}
+// {/*   Buttons below Tinder cards */}
+
+// {/*   Chat screen   */}
+//   Individual chat screen   */}
