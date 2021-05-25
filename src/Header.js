472714938c01f2cd5c87cc5df48d451593fa20ab
pjestioko <pauljoseph.estioko@gmail.com>
2021-05-24 19:26:28 -1000

    added iconbuttons

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from "@material-ui/core/IconButton";
 
 function Header(){
     return(
         // BEM 
         <div className="header">
-            <PersonIcon className="header__icon" fontSize="large" />
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large" />
+            </IconButton>
             <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-emblem.jpg" alt="tinder logo"/>
-            <ForumIcon className="header__icon" fontSize="large" />
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize="large" />
+            </IconButton>
         </div>
     )
 }
