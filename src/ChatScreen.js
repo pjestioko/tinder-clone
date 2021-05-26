c43640db72577a978126d064e4065aecffe72fee
pjestioko <pauljoseph.estioko@gmail.com>
2021-05-25 20:05:27 -1000

    user message fix

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -18,13 +18,19 @@ function ChatScreen() {
             name: 'Ellen',
             image: '...',
             message: 'HELLO?!',
+        },
+        {
+            message: 'Hi, Ellen. Sorry I am late to the party',
         }
     ])
     return (
         <div className="chatScreen">
-            <p className="chatScreen__timestamp">YOU MATCHED WITH ELLEN ON 06/10/2021</p>
+            <p className="chatScreen__timestamp">
+                YOU MATCHED WITH ELLEN ON 06/10/2021
+            </p>
             {messages.map((message) => (
-                <div className="chatScreen__message">
+                message.name ? (
+                    <div className="chatScreen__message">
                     <Avatar
                         className="chatScreen__image"
                         alt={message.name}
@@ -32,6 +38,11 @@ function ChatScreen() {
                     />
                     <p className="chatScreen__text">{message.message}</p>
                 </div>
+                ) : (
+                    <div className="chatScreen__message">
+                        <p className="chatScreen__textUser">{message.message}</p>
+                    </div>
+                )
             ))}
         </div>
     );
