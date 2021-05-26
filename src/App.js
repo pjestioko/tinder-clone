6dc3d560ce9ef64c2d1584df82cf9d3f41d22dea
pjestioko <pauljoseph.estioko@gmail.com>
2021-05-25 18:25:44 -1000

    added jsx back icon function

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
       <Router>
         <Switch>
           <Route path="/chat">
+            <Header backButton="/" />
             <h1>I am at chat page</h1>
           </Route>
           <Route path="/">
+            <Header />
             <TinderCards />
             <SwipeButtons />
           </Route>
--- src/Header.js
+++ src/Header.js
@@ -2,19 +2,32 @@ import React from 'react';
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
-import IconButton from "@material-ui/core/IconButton";
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Link, useHistory } from 'react-router-dom';
 
-function Header() {
+function Header({ backButton }) {
+    const history = useHistory();
     return (
         // BEM 
         <div className="header">
-            <IconButton>
-                <PersonIcon className="header__icon" fontSize="large" />
-            </IconButton>
-            <img className="header__logo" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ftous-logos.com%2Fwp-content%2Fuploads%2F2018%2F04%2Flogo-Tinder.png&f=1&nofb=1" alt="tinder logo" />
-            <IconButton>
-                <ForumIcon className="header__icon" fontSize="large" />
-            </IconButton>
+            {backButton ? (
+                <IconButton onClick={() => history.replace(backButton)}>
+                    <ArrowBackIosIcon fontSize="large" className="header__icon" />
+                </IconButton>
+            ) : (
+                <IconButton>
+                    <PersonIcon className="header__icon" fontSize="large" />
+                </IconButton>
+            )}
+            <Link to="/">
+                <img className="header__logo" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ftous-logos.com%2Fwp-content%2Fuploads%2F2018%2F04%2Flogo-Tinder.png&f=1&nofb=1" alt="tinder logo" />
+            </Link>
+            <Link to="/chat">
+                <IconButton>
+                    <ForumIcon className="header__icon" fontSize="large" />
+                </IconButton>
+            </Link>
         </div>
     )
 }
