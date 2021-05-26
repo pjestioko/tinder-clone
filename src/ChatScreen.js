aab94542c6b15dd6da60ead21cabf7cb0a9d7db2
pjestioko <pauljoseph.estioko@gmail.com>
2021-05-25 20:36:41 -1000

    input message logic

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -3,47 +3,65 @@ import Avatar from '@material-ui/core/Avatar'
 import './ChatScreen.css';
 
 function ChatScreen() {
+    const [input, setInput] = useState('')
     const [messages, setMessages] = useState([
         {
-            name: 'Ellen',
-            image: '...',
+            name: 'Zuck',
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.usmagazine.com%2Fwp-content%2Fuploads%2F2020%2F07%2FMark-Zuckerberg-Spooks-the-Internet-With-Too-Much-Sunscreen-on-His-Face-in-Hawaii-01.jpg%3Fcrop%3D557px%252C82px%252C896px%252C471px%26resize%3D1200%252C630%26ssl%3D1%26quality%3D86%26strip%3Dall&f=1&nofb=1',
             message: 'Whats up',
         },
         {
-            name: 'Ellen',
-            image: '...',
+            name: 'Zuck',
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.usmagazine.com%2Fwp-content%2Fuploads%2F2020%2F07%2FMark-Zuckerberg-Spooks-the-Internet-With-Too-Much-Sunscreen-on-His-Face-in-Hawaii-01.jpg%3Fcrop%3D557px%252C82px%252C896px%252C471px%26resize%3D1200%252C630%26ssl%3D1%26quality%3D86%26strip%3Dall&f=1&nofb=1',
             message: 'What are you doing?',
         },
         {
-            name: 'Ellen',
-            image: '...',
+            name: 'Zuck',
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.usmagazine.com%2Fwp-content%2Fuploads%2F2020%2F07%2FMark-Zuckerberg-Spooks-the-Internet-With-Too-Much-Sunscreen-on-His-Face-in-Hawaii-01.jpg%3Fcrop%3D557px%252C82px%252C896px%252C471px%26resize%3D1200%252C630%26ssl%3D1%26quality%3D86%26strip%3Dall&f=1&nofb=1',
             message: 'HELLO?!',
         },
         {
-            message: 'Hi, Ellen. Sorry I am late to the party',
+            message: 'Hi, Zuck. Sorry I am late to the party',
         }
-    ])
+    ]);
+
+    const handleSend = e => {
+        e.preventDefault();
+
+        setMessages([...messages, { message: input }]);
+        setInput("");
+    }
+
     return (
         <div className="chatScreen">
             <p className="chatScreen__timestamp">
-                YOU MATCHED WITH ELLEN ON 06/10/2021
+                YOU MATCHED WITH Zuck ON 06/10/2021
             </p>
             {messages.map((message) => (
                 message.name ? (
                     <div className="chatScreen__message">
-                    <Avatar
-                        className="chatScreen__image"
-                        alt={message.name}
-                        src={message.image}
-                    />
-                    <p className="chatScreen__text">{message.message}</p>
-                </div>
+                        <Avatar
+                            className="chatScreen__image"
+                            alt={message.name}
+                            src={message.image}
+                        />
+                        <p className="chatScreen__text">{message.message}</p>
+                    </div>
                 ) : (
                     <div className="chatScreen__message">
                         <p className="chatScreen__textUser">{message.message}</p>
                     </div>
                 )
             ))}
+            <form className="chatScreen__input">
+                <input
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                    className="chatScreen__inputField"
+                    placeholder="Type a message..."
+                    type="text" />
+                <button onClick={handleSend} type="submit" className="chatScreen__inputButton">SEND</button>
+            </form>
         </div>
     );
 }
--- src/Chats.js
+++ src/Chats.js
@@ -5,28 +5,28 @@ import './Chats.css';
 function Chats() {
     return <div className="chats">
         <Chat
-            name='Gigi'
+            name='Zuck'
             message='What are you doing today?'
             timeStamp='30 minutes ago'
-            profilePic='...'
+            profilePic='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.usmagazine.com%2Fwp-content%2Fuploads%2F2020%2F07%2FMark-Zuckerberg-Spooks-the-Internet-With-Too-Much-Sunscreen-on-His-Face-in-Hawaii-01.jpg%3Fcrop%3D557px%252C82px%252C896px%252C471px%26resize%3D1200%252C630%26ssl%3D1%26quality%3D86%26strip%3Dall&f=1&nofb=1'
         />
         <Chat
-            name='Jojo'
+            name='Doge'
             message='Hi, nice to meet you.'
             timeStamp='40 seconds ago'
-            profilePic='...'
+            profilePic='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.mycrafts.com%2Fi%2F1%2F7%2F100%2Fwow-doge-dog-meme-3d-drawing-iYVd-o.jpg&f=1&nofb=1'
         />
         <Chat
-            name='May'
+            name='Grumpy Cat'
             message='What are you doing tomorrow?'
             timeStamp='1 hour ago'
-            profilePic='...'
+            profilePic='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblogs-images.forbes.com%2Fclareoconnor%2Ffiles%2F2017%2F09%2Fgrumpy-cat-GettyImages-519250942-1200x800.jpg&f=1&nofb=1'
         />
         <Chat
-            name='Jayson'
-            message='Exactly! I was thinking the same.'
+            name='Barack'
+            message='Free for drinks this weekend?'
             timeStamp='55 minutes ago'
-            profilePic='...'
+            profilePic='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.wp.com%2Fwww.englishspeecheschannel.com%2Fwp-content%2Fuploads%2F2018%2F08%2FBarack-Obama.jpg%3Ffit%3D1920%252C1080%26ssl%3D1&f=1&nofb=1'
         />
     </div>;
 }
